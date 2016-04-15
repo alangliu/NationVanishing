62c4afd1e954e9d58ed760e0200fdbb3b1af97ec
zlailari <lailari.zeev@gmail.com>
2016-04-14 20:05:31 -0500

    started slider

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+// Call & init
+$(document).ready(function(){
+  $('.ba-slider').each(function(){
+    var cur = $(this);
+    // Adjust the slider
+    var width = cur.width()+'px';
+    cur.find('.resize img').css('width', width);
+  });
+});
